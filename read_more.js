d79c4aeb43a37e0d5ff5b29e95ad6c1c70208251
Matthew Hunter <matthewhunter@Matthews-MBP.attlocal.net>
2021-03-30 20:09:24 -0400

    adding read more buttun to each episode

--- read_more.js
+++ read_more.js
@@ -13,3 +13,67 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  function myFunction_two() {
+    var dots_two = document.getElementById("dots_two");
+    var moreText = document.getElementById("more_two");
+    var btnText = document.getElementById("myBtn_two");
+  
+    if (dots_two.style.display === "none") {
+      dots_two.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots_two.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunction_three() {
+    var dots_two = document.getElementById("dots_three");
+    var moreText = document.getElementById("more_three");
+    var btnText = document.getElementById("myBtn_three");
+  
+    if (dots_three.style.display === "none") {
+      dots_three.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots_three.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunction_four() {
+    var dots_two = document.getElementById("dots_four");
+    var moreText = document.getElementById("more_four");
+    var btnText = document.getElementById("myBtn_four");
+  
+    if (dots_four.style.display === "none") {
+      dots_four.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots_four.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunction_five() {
+    var dots_two = document.getElementById("dots_five");
+    var moreText = document.getElementById("more_five");
+    var btnText = document.getElementById("myBtn_five");
+  
+    if (dots_five.style.display === "none") {
+      dots_five.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots_five.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }

b29cd67d6b38f347d75c5628d48d33fd2493edf3
Matthew Hunter <matthewhunter@matthews-mbp.lan>
2021-04-02 05:23:29 -0400

    moved function out of contact form HTML

--- contact.js
+++ contact.js
@@ -2,3 +2,6 @@ function myFunction() {
     var elName = document.getElementById('name').value;
     alert (elName + ' :Thank you for your submision!');
   }
+
+  var elSubmit = document.getElementById('submit');
+  elSubmit.onclick = myFunction;

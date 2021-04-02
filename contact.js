0a7166be2de5a453d4076c54f0e7ed12d81b4d3c
Matthew Hunter <matthewhunter@matthews-mbp.lan>
2021-04-02 09:01:09 -0400

    changed wording on alert from contact form

--- contact.js
+++ contact.js
@@ -1,6 +1,6 @@
 function myFunction() {
     var elName = document.getElementById('name').value;
-    alert (elName + ' :Thank you for your submision!');
+    alert (elName + ', thank you for your submision!');
   }
 
   var elSubmit = document.getElementById('submit');
